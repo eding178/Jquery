59ac356089056f043368f4ad31aaae303120e4d7
Eduard Serra Tuneu <eding178@gmail.com>
2022-02-28 13:26:35 +0100

    game

--- APICalls.js
+++ APICalls.js
@@ -1,128 +1,151 @@
 
 var urlBase="http://localhost:57000/Empleados/";
+$("document").ready(function(){
+    ///////////
+    //GET BY ID
+    ///////////
+    $(".btn-get").click(function() { 
 
-///////////
-//GET BY ID
-///////////
-$(".btn-get").click(function() { 
-    $("#loader").fadeIn();
+        $("#loader").fadeIn();
 
-    $.ajax({ 
-        url: urlBase+"DetailsAPI/"+$("#in-text-get").val(),
-        type: 'GET',
-        dataType: "jsonp",
-        jsonpCallback: 'Method',
-        beforeSend: function (xhr) {
-            $("#loader").fadeIn();
-        },
-        success: function (data, textStatus) { 
-            $("#loader").fadeOut();
+        $.ajax({ 
+            url: urlBase+"DetailsAPI/"+$("#in-text-get").val(),
+            type: 'GET',
+            dataType: "jsonp",
+            jsonpCallback: 'Method',
+            beforeSend: function (xhr) {
+                $("#loader").fadeIn();
+            },
+            success: function (data, textStatus) { 
+                $("#loader").fadeOut();
 
-            $("#empleado").html(
-            "<ul>"+
-                "<li>ID: "+data.empleado.Id+"</li>"+
-                "<li>NOMBRE: "+data.empleado.Nombre+"</li>"+
-                "<li>EDAD: "+data.empleado.Edad+"</li>"+
-                "<li>ANTIGUEDAD: "+data.empleado.Antiguedad+"</li>"+
-                "<li>CATEGORIA: "+data.empleado.Categoria+"</li>"+
-            "</ul>"
-            )},
-        error(xhr,status,error){
-            $("#loader").fadeOut();
-            alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
-        }
+                $("#empleado").html(
+                "<ul>"+
+                    "<li>ID: "+data.empleado.Id+"</li>"+
+                    "<li>NOMBRE: "+data.empleado.Nombre+"</li>"+
+                    "<li>EDAD: "+data.empleado.Edad+"</li>"+
+                    "<li>ANTIGUEDAD: "+data.empleado.Antiguedad+"</li>"+
+                    "<li>CATEGORIA: "+data.empleado.Categoria+"</li>"+
+                "</ul>"
+                )},
+            error(xhr,status,error){
+                $("#loader").fadeOut();
+                alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
+            }
+        });
     });
-});
 
-///////////
-//GET ALL
-///////////
-$(".btn-get-all").click(function() { 
-    $("#loader").fadeIn();
-    $.ajax({ 
-        url: urlBase+"allAPI/",
-        type: 'GET',
-        dataType: "jsonp",
-        jsonpCallback: 'Method',
-        success: function (data, textStatus) { 
-            $("#loader").fadeOut();
-            $("#empleado").html("")
-                data.empleado.forEach( function(valor, indice, array) {
-                    $("#empleado").append(
-                        "<ul>"+
-                        "<li><button id=\""+valor.Id +"\" class=\"btn-del\">Borrar Empleado</button></li>"+
-                        "<li>ID: "+valor.Id+"</li>"+
-                        "<li>NOMBRE: "+valor.Nombre+"</li>"+
-                        "<li>EDAD: "+valor.Edad+"</li>"+
-                        "<li>ANTIGUEDAD: "+valor.Antiguedad+"</li>"+
-                        "<li>CATEGORIA: "+valor.Categoria+"</li>"+
-                        "</ul>"
-                    )
-                }
-            )
-        },
-        error(xhr,status,error){
-            $("#loader").fadeOut();
-            alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
-        }
+    ///////////
+    //GET ALL
+    ///////////
+    $(".btn-get-all").click(function() { 
+        $("#loader").fadeIn();
+        $.ajax({ 
+            url: urlBase+"allAPI/",
+            type: 'GET',
+            dataType: "jsonp",
+            jsonpCallback: 'Method',
+            success: function (data, textStatus) { 
+                $("#loader").fadeOut();
+                $("#empleado").html("")
+                    data.empleado.forEach( function(valor, indice, array) {
+                        $("#empleado").append(
+                            "<ul>"+
+                            "<li><button class=\"btn-del\">Borrar Empleado "+valor.Id+"</button></li>"+
+                            "<li>ID: "+valor.Id+"</li>"+
+                            "<li>NOMBRE: "+valor.Nombre+"</li>"+
+                            "<li>EDAD: "+valor.Edad+"</li>"+
+                            "<li>ANTIGUEDAD: "+valor.Antiguedad+"</li>"+
+                            "<li>CATEGORIA: "+valor.Categoria+"</li>"+
+                            "</ul>"
+                        )
+                    }
+                )
+            },
+            error(xhr,status,error){
+                $("#loader").fadeOut();
+                alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
+            }
+        });
     });
-});
 
-///////////
-//UPDATE BY ID
-///////////
-$(".btn-update").click(function() { 
-    $("#loader").fadeIn();
-    $.ajax({ 
-        url: urlBase+"EditAPI/"+$("#Id-edit").val(),
-        type: 'GET',
-        data: {
-            Id:$("#Id-edit").val(),
-            Nombre:$("#Nombre-edit").val(),
-            Edad:$("#Edad-edit").val(),
-            Antiguedad:$("#Antiguedad-edit").val(),
-            Categoria:$("#Categoria-edit").val()
-        },
-        dataType: "jsonp",
-        jsonpCallback: 'Method',
-        cors: true ,
-        contentType: false,
-        secure: true,
-        xhrFields: { withCredentials: true },
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            "accept": "application/json",
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader ("Authorization", "Basic " + btoa(""));
-        },
-        success: function (data, textStatus) { 
-            $("#loader").fadeOut();
-        }, 
-        error(xhr,status,error){
-            alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
-            $("#loader").fadeOut();
+    ///////////
+    //UPDATE BY ID
+    ///////////
+    $(".btn-update").click(function() { 
+        $("#loader").fadeIn()
+        $.ajax({ 
+            url: urlBase+"EditAPI/"+$("#Id-edit").val(),
+            type: 'GET',
+            data: {
+                Id:$("#Id-edit").val(),
+                Nombre:$("#Nombre-edit").val(),
+                Edad:$("#Edad-edit").val(),
+                Antiguedad:$("#Antiguedad-edit").val(),
+                Categoria:$("#Categoria-edit").val()
+            },
+            dataType: "jsonp",
+            jsonpCallback: 'Method',
+            cors: true ,
+            contentType: false,
+            secure: true,
+            xhrFields: { withCredentials: true },
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                "accept": "application/json",
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader ("Authorization", "Basic " + btoa(""));
+            },
+            success: function (data, textStatus) { 
+                $("#loader").fadeOut();
+            }, 
+            error(xhr,status,error){
+                //alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
+                $("#loader").fadeOut();
 
-        }
+            }
+        });
     });
-});
 
-//////////////
-//DELETE BY ID
-//////////////
+    //////////////
+    //DELETE BY ID
+    //////////////
+    $(".btn-del").click(function(data) { 
+        console.log("entra")
+        $("#loader").fadeIn();
+        console.log("data: "+data +$("this").val())//hauria de servir per fer la URL amb la ID
+        /*
+        $.ajax({ 
+            url: urlBase+"DetailsAPI/"+$("#").val(),
+            type: 'GET',
+            dataType: "jsonp",
+            jsonpCallback: 'Method',
+            beforeSend: function (xhr) {
+                $("#loader").fadeIn();
+            },
+            success: function (data, textStatus) { 
+                $("#loader").fadeOut();
+            },
+            error(xhr,status,error){
+                $("#loader").fadeOut();
+                alert("something went wrong.\n\nError: "+error+"\nStatus: "+status+"\nXHR: "+xhr);
+            }
+        });*/
+    });
 
 
 
 
+    function EmpleadoToJSON(Id, Nombre, Edad, Antiguedad, Categoria) {
+        return "{\"Empleado\": {\"Id\": \"" + Id + "\",\"Nombre\": \"" + Nombre + "\",\"Edad\": \"" + Edad + "\",\"Antiguedad\": \"" + Antiguedad + "\",\"Categoria\": \"" + Categoria + "\"}}";
+    }
 
-function EmpleadoToJSON(Id, Nombre, Edad, Antiguedad, Categoria) {
-    return "{\"Empleado\": {\"Id\": \"" + Id + "\",\"Nombre\": \"" + Nombre + "\",\"Edad\": \"" + Edad + "\",\"Antiguedad\": \"" + Antiguedad + "\",\"Categoria\": \"" + Categoria + "\"}}";
-}
-
-//CALLBACK
-Method = function(data) {
-    successResponse(data);
-}
-   successResponse = function(data) {
-   //functionality goes here;
-}
+    //CALLBACK
+    Method = function(data) {
+        successResponse(data);
+    }
+    successResponse = function(data) {
+    //functionality goes here;
+    }
+})
--- JS.js
+++ JS.js
@@ -24,7 +24,117 @@ $("document").ready(function(){
         $("p.pAlert").append(" more text here ")
     })
 
+    const tiempoTranscurrido = Date.now();
+    const hoy = new Date(tiempoTranscurrido);
+    $("#time-now").append(hoy.toDateString())
 
+///////////
+//problema logic piramide d'*
+///////////
+$("#btn-piramide").click(function(){
+    var rows = $("#piramide-input").val()
+    for (var j = 0; j < rows; j++) {
+        $("#piramide").append("<br/>");
+        for (var i = 0; i <= j; i++) {
+            
+            $("#piramide").append("*");
+        }
+    }
+})
+
+
+$(function(){
+    setInterval(oneSecondFunction, 500);
+    });
     
+    function oneSecondFunction() {
+        var pxMove = 20
+        var rndProb = randomIntFromInterval(1, 10)
+
+    if(rndProb <= 0){//max:9
+        var topOleft = randomIntFromInterval(1, 2)
+
+        //miro la meva posicio
+        MyArr = $("#showroom").attr('style').split("top: ")[1].slice(0,-3).split(" ")
+        var myLeft=MyArr[0].slice(0,-3)
+        var mytop = MyArr[2]
+        console.log(myLeft+" "+mytop)
+
+        //miro la seva posicio
+        //console.log($("#showroomOther").attr('style').split("top: ")[1].slice(0,-3).split(" "))
+        var arrOther =  $("#showroomOther").attr('style').split("top:")[1].slice(0,-3).split("px;left:")
+        var hisLeft=arrOther[0].slice(0,-3)
+        var histop = arrOther[2]
+        if(topOleft == 1){
+            if(mytop<histop){
+                $("#showroomOther").animate({
+                    top: "-="+pxMove+"px"
+                },"fast");
+            }else{
+                $("#showroomOther").animate({
+                    top: "+="+pxMove+"px"
+                },"fast");
+            }
+            if(myLeft<hisLeft){
+                $("#showroomOther").animate({
+                    left: "+="+pxMove+"px"
+                },"fast");
+            }else{
+                $("#showroomOther").animate({
+                    left: "-="+pxMove+"px"
+                },"fast");
+            }
+        }
+
+        
+    }else{
+        var rnd = randomIntFromInterval(37, 40)
+        if (rnd == 37) {
+            $("#showroomOther").animate({
+                left: "-="+pxMove+"px"
+            },"fast");
+        } else if (rnd == 38) {
+            $("#showroomOther").animate({
+                top: "-="+pxMove+"px"
+            },"fast");
+        } else if (rnd == 39) {
+            $("#showroomOther").animate({
+                left: "+="+pxMove+"px"
+            },"fast");
+        } else if (rnd == 40) {
+            $("#showroomOther").animate({
+                top: "+="+pxMove+"px"
+            },"fast");
+        }
+    }
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+      
+///////////
+//DIV on move
+///////////
     
+    $(window).bind('keydown', function(e){
+        if (e.keyCode == 37) {
+            $("#showroom").animate({
+                left: "-=20px"
+            },"fast");
+        } else if (e.keyCode == 38) {
+            $("#showroom").animate({
+                top: "-=20px"
+            },"fast");
+        } else if (e.keyCode == 39) {
+            $("#showroom").animate({
+                left: "+=20px"
+            },"fast");
+        } else if (e.keyCode == 40) {
+            $("#showroom").animate({
+                top: "+=20px"
+            },"fast");
+        }
+        //console.log($("#showroom").attr('style'))
+    });
 })
